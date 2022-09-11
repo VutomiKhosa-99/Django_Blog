7cf8f6b8d75faf286aadf9ecc15902a6bd182016
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2022-09-11 08:03:50 +0200

    k

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MDEDIA_URL = '/media/'
 
