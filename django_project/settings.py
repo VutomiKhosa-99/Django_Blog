b1b49395996837c1ccb6093b31fc83f5dd6dbb27
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2022-09-11 10:29:06 +0200

    h

--- django_project/settings.py
+++ django_project/settings.py
@@ -158,4 +158,4 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
